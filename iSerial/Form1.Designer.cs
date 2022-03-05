ff782c9f96fc346722ffe5ce1b6b04322302d335
longg <longg@WINEGI-DESKTOP>
2022-03-06 02:13:48 +0900

    プロジェクト ファイルを追加します。

---
+++ iSerial/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace iSerial
+{
+    partial class iSerial
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.btnScan = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.tbxRxData = new System.Windows.Forms.TextBox();
+            this.cmbCOMPort = new System.Windows.Forms.ComboBox();
+            this.tbxTxData = new System.Windows.Forms.TextBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnScan
+            // 
+            this.btnScan.Location = new System.Drawing.Point(12, 12);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(75, 23);
+            this.btnScan.TabIndex = 0;
+            this.btnScan.Text = "ReScan";
+            this.btnScan.UseVisualStyleBackColor = true;
+            this.btnScan.Click += new System.EventHandler(this.BtnScan_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(314, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.BtnOpen_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(395, 12);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(713, 11);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(713, 417);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 4;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.BtnSend_Click);
+            // 
+            // tbxRxData
+            // 
+            this.tbxRxData.Location = new System.Drawing.Point(12, 41);
+            this.tbxRxData.Multiline = true;
+            this.tbxRxData.Name = "tbxRxData";
+            this.tbxRxData.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbxRxData.Size = new System.Drawing.Size(776, 190);
+            this.tbxRxData.TabIndex = 5;
+            // 
+            // cmbCOMPort
+            // 
+            this.cmbCOMPort.FormattingEnabled = true;
+            this.cmbCOMPort.Location = new System.Drawing.Point(93, 14);
+            this.cmbCOMPort.Name = "cmbCOMPort";
+            this.cmbCOMPort.Size = new System.Drawing.Size(215, 20);
+            this.cmbCOMPort.TabIndex = 6;
+            this.cmbCOMPort.Text = "- Select COM Port -";
+            // 
+            // tbxTxData
+            // 
+            this.tbxTxData.Location = new System.Drawing.Point(12, 419);
+            this.tbxTxData.Name = "tbxTxData";
+            this.tbxTxData.Size = new System.Drawing.Size(695, 19);
+            this.tbxTxData.TabIndex = 7;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.SerialPort1_DataReceived);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 237);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(776, 176);
+            this.chart1.TabIndex = 8;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.Chart1_Click);
+            // 
+            // iSerial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.tbxTxData);
+            this.Controls.Add(this.cmbCOMPort);
+            this.Controls.Add(this.tbxRxData);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnScan);
+            this.Name = "iSerial";
+            this.Text = "iSerial";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnScan;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox tbxRxData;
+        private System.Windows.Forms.ComboBox cmbCOMPort;
+        private System.Windows.Forms.TextBox tbxTxData;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ iSerial/Form1.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace iSerial
+{
+
+    public partial class iSerial : Form
+    {
+
+        Random rdm = new Random(); /* 乱数 */
+
+        Series seriesLine = new Series();
+        Series seriesLine2 = new Series();
+        private int index = 0;
+        private int yIndex = 0;
+
+        bool chartOptimized = false;
+
+        List<int> startList = new List<int>();
+        List<int> endList = new List<int>();
+
+        List<ChartArea> chartAreas = new List<ChartArea>();
+        List<Series> chartSeries = new List<Series>();
+
+        public iSerial()
+        {
+            InitializeComponent();
+            ScanCOMPorts();
+
+            InitCharts();
+        }
+
+        private void InitCharts()
+        {
+            /* 初期値の削除 */
+            chart1.Series.Clear();
+            chart1.ChartAreas.Clear();
+            chart1.Titles.Clear();
+
+            //seriesLine.ChartType = SeriesChartType.Line;
+            //seriesLine.LegendText = "Legend:Line";
+
+            //seriesLine2.ChartType = SeriesChartType.Line;
+            //seriesLine2.LegendText = "Legend:Line2";
+            //seriesLine.BorderWidth = 2;
+            //seriesLine.MarkerStyle = MarkerStyle.Circle;
+            //seriesLine.MarkerSize = 12;
+
+            //for (int i = 0; i < 10; i++)
+            //{
+                //seriesLine2.Points.Add(new DataPoint(i, rdm.Next(0, 210)));
+            //}
+
+            //ChartArea area1 = new ChartArea("area1");
+            //ChartArea area2 = new ChartArea("area2");
+
+            //seriesLine2.ChartArea = "area2";
+
+            //chart1.ChartAreas.Add(area1);
+            //chart1.ChartAreas.Add(area2);
+            //chart1.Series.Add(seriesLine);
+            //chart1.Series.Add(seriesLine2);
+        }
+
+
+
+        private void ScanCOMPorts()
+        {
+            cmbCOMPort.Items.Clear();
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string p in ports)
+            {
+                cmbCOMPort.Items.Add(p);
+            }
+        }
+
+        private void BtnScan_Click(object sender, EventArgs e)
+        {
+            ScanCOMPorts();
+        }
+
+        private void BtnOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.PortName = cmbCOMPort.Text; // COM名設定
+                serialPort1.Open();                     // ポート接続
+                btnOpen.Enabled = false;                // 接続　Off
+                btnClose.Enabled = true;                // 切断　On
+                btnScan.Enabled = false;                // 更新　Off
+                cmbCOMPort.Enabled = false;             // COMリスト　Off
+                btnSend.Enabled = true;                 // 送信　On
+                tbxRxData.Clear();                      // 画面クリア
+                tbxRxData.AppendText("Connected\r\n");  // 接続と表示
+            }
+            catch
+            {
+                BtnClose_Click(this, null);     // 切断ボタンを押す
+            }
+        }
+
+        // TODO: Chartとの組み合わせでうまく動いていない部分があるっぽい
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            btnOpen.Enabled = true;             // 接続　On
+            btnClose.Enabled = false;           // 切断　Off
+            btnScan.Enabled = true;             // 更新　On
+            cmbCOMPort.Enabled = true;          // COMリスト　On
+            btnSend.Enabled = false;            // 送信　Off
+            try
+            {
+                serialPort1.DiscardInBuffer();  // 入力バッファを破棄
+                serialPort1.DiscardOutBuffer(); // 出力バッファを破棄
+                serialPort1.Close();             // COMポートを閉じる
+            }
+            catch { };
+        }
+
+        private void BtnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.Write(tbxTxData.Text + "\r\n");
+            }
+            catch
+            {
+                BtnClose_Click(this, null);
+            }
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            tbxRxData.Clear();
+        }
+
+        private void SerialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (serialPort1.BytesToRead < 2)
+            {
+                return;
+            }
+
+            string recievedData;
+            if(!serialPort1.IsOpen) return;
+            try {
+                recievedData = serialPort1.ReadLine();
+            }
+            catch
+            {
+                return ;
+            }
+            index++;/* 読み込みまで出来たら番号を増やす */
+            try
+            {
+                SetText(recievedData);
+            }catch{
+                BtnClose_Click(this, null);
+            }
+
+            try
+            {
+                string[] splitedData = recievedData.Replace("\r", "").Replace("\n", "").Split(',');
+
+                /* 初期化がまだの場合、ここで文解析 */
+                if (index == 3 && !chartOptimized)
+                {
+                    System.Diagnostics.Debug.WriteLine("data for optimize");
+                    System.Diagnostics.Debug.WriteLine(recievedData);
+                    OptimizeCharts(splitedData);
+                }else if (chartOptimized) {
+                    UpdateCharts2(splitedData);
+                }
+                //if (splitedText[0] != "") UpdateCharts(index, Convert.ToDouble(splitedText[0]));
+                //if (splitedData[1] != "") UpdateCharts(index, Convert.ToDouble(splitedData[1]));
+                //if (splitedText[2] != "") UpdateCharts(index, Convert.ToDouble(splitedText[2]));
+
+            }
+            catch
+            {
+                return ;
+            }
+
+        }
+
+        delegate void SetTextCallback(string text);
+        private void SetText(string text)
+        {
+            if (tbxRxData.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                BeginInvoke(d, new object[] { text });
+            }
+            else
+            {
+                tbxRxData.AppendText(text+"\n");
+            }
+        }
+
+        delegate void UpdateChartsCallback(double x, double y);
+        private void UpdateCharts(double x, double y)
+        {
+            if (chart1.InvokeRequired){
+                UpdateChartsCallback d = new UpdateChartsCallback(UpdateCharts);
+                BeginInvoke(d, new object[] { x, y });
+            }else{
+                seriesLine.Points.Add(new DataPoint(x, y));
+                chart1.ChartAreas["area1"].AxisX.Minimum = x - 100;
+            }
+        }
+
+        delegate void UpdateCharts2Callback(string[] splitedData);
+        private void UpdateCharts2(string[] splitedData)
+        {
+            if (chart1.InvokeRequired)
+            {
+                UpdateCharts2Callback d = new UpdateCharts2Callback(UpdateCharts2);
+                BeginInvoke(d, new object[] { splitedData });
+            }
+            else
+            {
+                int currentChartArea = -1;
+                int currentSeries = -1;
+                foreach(var val in splitedData)
+                {
+                    if (val.Equals("<"))
+                    {
+                        currentChartArea++;
+                    }
+                    else if (val.Equals(">"))
+                    {
+                        chart1.ChartAreas["cArea" + currentChartArea.ToString()].AxisX.Minimum = yIndex - 100;
+                    }
+                    else if (currentChartArea != -1) /* -1だとareaがないのでスルー */
+                    {
+                        currentSeries++;
+                        chartSeries[currentSeries].Points.Add(new DataPoint(yIndex, Convert.ToDouble(val)));
+                        yIndex++;
+                    }
+                    else
+                    {
+                    }
+                }
+                //seriesLine.Points.Add(new DataPoint(splitedData);
+            }
+        }
+
+        delegate void OptimizeChartsCallback(string[] splitedData);
+
+        private void OptimizeCharts(string[] splitedData)
+        {
+            if (chart1.InvokeRequired)
+            {
+                System.Diagnostics.Debug.WriteLine("OPT2");
+                OptimizeChartsCallback d = new OptimizeChartsCallback(OptimizeCharts);
+                Invoke(d, new object[] { splitedData });
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("OPT");
+                /* <と>がどれだけあるかの判別 */
+                int num = -1;
+                foreach (string s in splitedData)
+                {
+                    num++;
+                    if(s.Equals("<")) startList.Add(num);
+                    if(s.Equals(">")) endList.Add(num);
+                }
+                if (startList.Count != endList.Count) chartOptimized =  false; /* <と>の数が違うとき */
+
+                int currentChartArea = -1;
+                /* <>の数に併せてchartを作成 */
+
+                foreach (var val in splitedData) {
+                    System.Diagnostics.Debug.WriteLine("|");
+                    System.Diagnostics.Debug.WriteLine(val);
+                    if (val.Equals("<")) {
+                        currentChartArea++;
+                        ChartArea cA = new ChartArea("cArea" + currentChartArea.ToString());
+                        chart1.ChartAreas.Add(cA);
+                    }
+                    else if (val.Equals(">"))
+                    {
+                        /* 閉じかっこの時（何もしない） */
+                    }
+                    else if(currentChartArea != -1 && val != "") /* -1だとareaがないのでスルー */
+                    {
+                        var s = new Series();
+                        s.ChartType = SeriesChartType.Line;
+                        s.LegendText = "Legend:Line";
+                        s.ChartArea = "cArea" + currentChartArea.ToString();
+                        chartSeries.Add(s);
+                        chart1.Series.Add(s);
+                        System.Diagnostics.Debug.WriteLine(chartSeries.Count);
+                    }
+                    else
+                    {
+                    }
+                }
+                System.Diagnostics.Debug.WriteLine("checking");
+                System.Diagnostics.Debug.WriteLine((currentChartArea + 1) * 2 + chartSeries.Count);
+                System.Diagnostics.Debug.WriteLine(chartSeries.Count);
+                System.Diagnostics.Debug.WriteLine("/");
+                System.Diagnostics.Debug.WriteLine(splitedData.Length);
+
+                if ((currentChartArea+1)*2 + chartSeries.Count == splitedData.Length) {
+                    chartOptimized = true;
+                }
+            }
+        }
+
+        private void Chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
